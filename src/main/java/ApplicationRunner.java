72931b53fc672789467c12e813d431b05a549371
kozlovnikita <kozlovnikita@MacBook-Pro-Kozlov.local>
2024-12-26 23:46:02 +0300

    добавлены constructor injection

--- src/main/java/ApplicationRunner.java
+++ src/main/java/ApplicationRunner.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class ApplicationRunner {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
-        System.out.println(context.getBean("Connection1"));
+        ConnectionPool connection1 = context.getBean("Connection1", ConnectionPool.class);
+        System.out.println(connection1);
     }
 }
